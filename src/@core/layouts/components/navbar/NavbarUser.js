e74ff4946de8dd1c3fa3a9c727a37006b8977e50
VAIBHAV <18btrct046@jainuniversity.ac.in>
2023-01-23 12:10:07 +0530

    navbar update

--- src/@core/layouts/components/navbar/NavbarUser.js
+++ src/@core/layouts/components/navbar/NavbarUser.js
@@ -1,9 +1,9 @@
 // ** Dropdowns Imports
 import IntlDropdown from './IntlDropdown'
-import CartDropdown from './CartDropdown'
+
 import UserDropdown from './UserDropdown'
 import NavbarSearch from './NavbarSearch'
-import NotificationDropdown from './NotificationDropdown'
+
 
 // ** Third Party Components
 import { Sun, Moon } from 'react-feather'
@@ -33,8 +33,7 @@ const NavbarUser = props => {
         </NavLink>
       </NavItem>
       <NavbarSearch />
-      <CartDropdown />
-      <NotificationDropdown />
+     
       <UserDropdown />
     </ul>
   )
